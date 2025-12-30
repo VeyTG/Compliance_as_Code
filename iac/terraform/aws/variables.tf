2565b2dd702c1c5459cc765ff8c9733dd84c9efc
VeyTG <vutuongvy0202@gmail.com>
2025-12-30 20:21:00 +0700

    fix: terraform fmt and validate issues

--- iac/terraform/aws/variables.tf
+++ iac/terraform/aws/variables.tf
@@ -47,3 +47,11 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+locals {
+  common_tags = {
+    Project     = "AWS-Compliance"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
